115c31176d47b1f13a9ab22ba2a2058a00319441
HarshVora242 <hvora6@hawk.iit.edu>
2019-09-23 23:39:22 -0500

    modified

--- read.java
+++ read.java
@@ -7,22 +7,43 @@ public class read
 {
 		
 public static void main(String[] args) throws IOException,ClassNotFoundException  
-{ 
-
-        
+{  
        
 	try {       
-	    File f = new File("/home/sathyaveer27/Desktop/ABC");
+		
+            Scanner scn = new Scanner(System.in); 
+	 
+	    File f = new File("/home/harsh/Desktop/10kbfiles/");
             Socket socket = new Socket("127.0.0.1",9999);
-           
+	    ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
 	    ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list()));
 	    System.out.println(names.toString());
-    
-       
-          
-                    ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+	   
                     objectOutput.writeObject(names);
-               }
+	    ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream()); 
+	 while (true)  
+            { 
+                System.out.println(objectInput.readUTF()); 
+                String tosend = scn.nextLine(); 
+                objectOutput.writeUTF(tosend); 
+                  
+                // If client sends exit,close this connection  
+                // and then break from the while loop 
+                if(tosend.equals("2")) 
+                { 
+                    System.out.println("Closing this connection : " + socket); 
+                    socket.close(); 
+                    System.out.println("Connection closed"); 
+                    break; 
+                } 
+                  
+                // printing date or time as requested by client 
+                String received = objectInput.readUTF(); 
+                System.out.println(received); 
+            } 
+
+
+          }
 		catch (Exception e) {
                     System.out.println("The title list has not come from the server");
                     e.printStackTrace();
